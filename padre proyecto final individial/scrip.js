1c6dfdfbdc141b53bb3cfc56678750e27df49268
tomasdelrosariosuero-eng <tomasdelrosariosuero@gmail.com>
2026-04-17 10:24:04 -0400

    Add files via upload

---
+++ padre proyecto final individial/scrip.js
@@ -0,0 +1,14 @@
+function info(car) {
+
+    if (car === "m4") {
+        alert("BMW M4: Lujo, tecnología y potencia 🔥");
+    }
+
+    if (car === "supra") {
+        alert("Supra Mk4: Motor 2JZ, leyenda del tuning 🚗");
+    }
+
+    if (car === "skyline") {
+        alert("Skyline GT-R: Dominante en carreras 💨");
+    }
+}
